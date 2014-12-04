80a5e7eb9a93b8b03dd2c80913322424d6036439
dnjs0228 <dnjsrud0228@naver.com>
2014-12-04 15:37:20 +0900

    ss

--- sketch.js
+++ sketch.js
@@ -1,7 +1,11 @@
-var bg;
-
 function setup() {
- 
-  bg = loadImage("mkw_89095.jpeg");
-  createCanvas(640, 960);
+  createCanvas(640, 480);
+  background(99, 2, 2);
+  // background(red, green, blue);
+  stroke(255);
+  noFill();
+
+  line(10, 10, 90, 10); // x1, y1, x2, y2
+    rect(20, 20, 80, 50); // x, y, width, height
+    ellipse(50, 50, 50, 50); // centerX, centerY, width, height
 }
