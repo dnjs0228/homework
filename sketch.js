2aea86c34a258535de74f98ab1d98feb9b6387e4
dnjs0228 <dnjsrud0228@naver.com>
2014-12-04 16:13:50 +0900

    ss

--- sketch.js
+++ sketch.js
@@ -1,8 +1,22 @@
 function setup() {
+  createCanvas(640, 480);
+  background(99, 2, 2);
+  // background(red, green, blue);
+  stroke(255);
+  noFill();
+}
 
-    createCanvas(1000,600);
-    bg = loadImage("bg.png");
 
-      noFill();
-    noStroke();
+function draw() {
+  //background(246, 153, 136);
+//for (시작상태; 반복하는 조건; 증가값)
+  stroke(134, 153, 184)   
+  for (var x = 1; x <=5; x++)
+  {
+    ellipse(x * 400 + 1300, x  * 30 + 1450, x * 450, x * 45);
+  }
+  stroke(250, 217, 20)  
+  var size = Math.abs(mouseX - 320);
+ellipse(320, 240, size, size);
+  //ellipse(320, 240, mouseX, mouseY);
 }
