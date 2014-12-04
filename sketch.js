f161c4e6c1b96aeabd9857498bbcd68398fe2bd7
dnjs0228 <dnjsrud0228@naver.com>
2014-12-04 16:10:52 +0900

    ss

--- sketch.js
+++ sketch.js
@@ -1,11 +1,8 @@
 function setup() {
-  createCanvas(640, 480);
-  background(99, 2, 2);
-  // background(red, green, blue);
-  stroke(255);
-  noFill();
 
-  line(10, 10, 90, 10); // x1, y1, x2, y2
-    rect(20, 20, 80, 50); // x, y, width, height
-    ellipse(50, 50, 50, 50); // centerX, centerY, width, height
+    createCanvas(1000,600);
+    bg = loadImage("bg.png");
+
+      noFill();
+    noStroke();
 }
